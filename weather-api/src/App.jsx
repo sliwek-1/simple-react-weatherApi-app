c755ec58e5b3d6c77cb23a72699e5afc073f1a89
sliwek-1 <msliwinski247@gmail.com>
2024-01-14 14:51:15 +0100

    #

--- weather-api/src/App.jsx
+++ weather-api/src/App.jsx
@@ -24,6 +24,7 @@ export function App() {
       let response = await request.json();
       setDataValue(response)
       setCityName(inputValue)
+      console.log(response)
     }catch(error){
       console.log(error)
     }
--- weather-api/src/cart.jsx
+++ weather-api/src/cart.jsx
@@ -1,6 +1,6 @@
 import { Spinner, Card } from "react-bootstrap"
 
-export function Cart({items , name}) {
+export function Cart({items , name}) { 
     return (
         <>
             {items.error != null ? 
@@ -8,9 +8,19 @@ export function Cart({items , name}) {
                 <span className="visually-hidden">Loading...</span>
             </Spinner> : 
             <Card>
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>Odczuwalna Temperatura {items.feels_like}</Card.Text>
+                <Card.Body style={{width: "50vw"}} className="d-flex jusfy-content-around align-items-center flex-row">
+                    <div style={{width: "35vw"}}>
+                        <Card.Title className="c-black">{name}</Card.Title>
+                        <div className="text-muted" >
+                            <Card.Text>Temperatura w ciągu dnia: {items.max_temp} / {items.min_temp}°C</Card.Text>
+                            <Card.Text>Odczuwalna Temperatura {items.feels_like}</Card.Text>
+                            <Card.Text>Wilgotność {items.humidity}%</Card.Text>
+                            <Card.Text>Zachmurzenie {items.cloud_pct}%</Card.Text>
+                        </div>
+                    </div>
+                    <div style={{width: "15vw"}}>
+                        <Card.Title className="m-3" style={{fontSize: "5rem"}}>{items.temp}°C</Card.Title>
+                    </div>
                 </Card.Body>
             </Card>}
         </>
